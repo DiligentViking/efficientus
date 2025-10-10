5b4d545d2df29622d8e26d79fb20e7922f74ea7e
DiligentViking <acore-n@proton.me>
2025-10-10 12:49:08 -0500

    Tweak logging convention; Revise readAllTodos func

--- src/todo.js
+++ src/todo.js
@@ -12,11 +12,12 @@ const createTodo = (lists, desc, doDate, priority) => {
     todoId,
     JSON.stringify(todoData)
   );
-
   console.log(`Todo "${desc.slice(0, 5)}..." (${todoId}) created.`);
 }
 
 const readAllTodos = (list) => {
+  const todosInList = [];
+
   console.log(`Todos for "${list}":`);
   for (let x = 0; x < localStorage.length; x++) {
     const data = localStorage.getItem(x);
@@ -24,8 +25,11 @@ const readAllTodos = (list) => {
 
     if (todo.lists.includes(list)) {
       console.log(todo);
+      todosInList.push(todo);
     }
   }
+
+  return todosInList;
 }
 
 export { createTodo, readAllTodos };
